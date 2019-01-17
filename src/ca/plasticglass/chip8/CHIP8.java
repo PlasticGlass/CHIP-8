579ef593fdb87aadb7e5fbf69972dc4f7054b558
Zubair <triangularwheels@gmail.com>
2019-01-16 19:33:36 -0500

    refactoring

--- src/ca/plasticglass/chip8/CHIP8.java
+++ src/ca/plasticglass/chip8/CHIP8.java
@@ -1,8 +1,7 @@
 package ca.plasticglass.chip8;
+
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.image.*;
+
 /**
  * Created by Zubair Waheed on 3/13/2018.
  */
@@ -47,87 +46,34 @@ import java.awt.image.*;
 
         }
     }*/
-    public class CHIP8 extends JPanel {
+public class CHIP8 {
 
-    private BufferedImage canvas;
+    private CPU cpu;
+    private Memory memory;
+    private Screen screen;
+    private Keyboard keyboard;
+    private JFrame display;
 
-    public CHIP8(int width, int height) {
-        canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        fillCanvas(Color.BLUE);
-
-        drawRect(Color.RED, 20, 20, 10, 10);
+    public CHIP8() {
+        initSystem();
+        initGUI();
     }
 
-    public Dimension getPreferredSize() {
-        return new Dimension(canvas.getWidth(), canvas.getHeight());
+    private void initSystem() {
+        this.keyboard = new Keyboard();
+        this.memory = new Memory();
+        this.screen = new Screen();
+        this.cpu = new CPU(memory, screen, keyboard);
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(canvas, null, null);
-    }
-
-
-    public void fillCanvas(Color c) {
-        int color = c.getRGB();
-        for (int x = 0; x < canvas.getWidth(); x++) {
-            for (int y = 0; y < canvas.getHeight(); y++) {
-                canvas.setRGB(x, y, color);
-            }
-        }
-        repaint();
-    }
-
-
-    public void drawLine(Color c, int x1, int y1, int x2, int y2) {
-        // Implement line drawing
-        repaint();
-    }
-
-    public void drawRect(Color c, int x1, int y1, int width, int height) {
-        int color = c.getRGB();
-        // Implement rectangle drawing
-        for (int x = x1; x < x1 + width; x++) {
-            for (int y = y1; y < y1 + height; y++) {
-                canvas.setRGB(x, y, color);
-            }
-        }
-        repaint();
-    }
-
-    public void drawOval(Color c, int x1, int y1, int width, int height) {
-        // Implement oval drawing
-        repaint();
-    }
-
-
-    public static void main(String[] args) {
-        int width = 640;
-        int height = 320;
-        JFrame frame = new JFrame("Direct draw demo");
-        Keyboard k = new Keyboard();
-
-        // CHIP8 panel = new CHIP8(width, height);
-        Screen panel = new Screen();
-
-        frame.add(panel);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.addKeyListener(k);
-
-        for (int i = 0; i < 64; i++) {
-            //if(i%2 == 0) {
-                panel.redrawPixel(i, 0, 1);
-                try {
-                    Thread.sleep(100);
-                } catch (Exception ex) {
-                }
-           // }
-
-        }
+    private void initGUI() {
+        this.display = new JFrame("CHIP-8");
+        this.display.add(screen);
+        this.display.pack();
+        this.display.setVisible(true);
+        this.display.setResizable(false);
+        this.display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.display.addKeyListener(keyboard);
     }
 }
 
---
+++ src/ca/plasticglass/chip8/Emulator.java
@@ -0,0 +1,18 @@
+package ca.plasticglass.chip8;
+
+public class Emulator {
+    public static void main(String[] args) {
+        CHIP8 emulator = new CHIP8();
+
+        /*for (int i = 0; i < 64; i++) {
+            //if(i%2 == 0) {
+            chip8.screen.redrawPixel(i, 0, 1);
+                try {
+                    Thread.sleep(100);
+                } catch (Exception ex) {
+                }
+           // }
+
+        }*/
+    }
+}
